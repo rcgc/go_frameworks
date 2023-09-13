56de9eac8d7b21aa317f71ebf66dd5f70bf418fb
rcgc <roberto140298@gmail.com>
2023-09-13 13:43:25 -0600

    go_kratos finished

---
+++ go_kratos/go.mod
@@ -0,0 +1,21 @@
+module example.com/go_kratos
+
+go 1.19
+
+require (
+	github.com/go-kratos/kratos/v2 v2.7.0
+	github.com/gorilla/mux v1.8.0
+)
+
+require (
+	github.com/go-kratos/aegis v0.2.0 // indirect
+	github.com/go-playground/form/v4 v4.2.0 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	golang.org/x/sync v0.3.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230629202037-9506855d4529 // indirect
+	google.golang.org/grpc v1.56.1 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ go_kratos/server_middleware.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"github.com/go-kratos/kratos/v2"
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware"
+)
+
+/*
+	Contians middlewares
+	Service discovery
+	Logging
+	Monitoring
+
+	Suitable for production microservices
+
+	Supports call using gRPC, HTTP and
+	anyother protocol
+
+	For large-scale microservices
+
+	Distributed systems
+
+	High-performance microservices
+
+	API gateways
+
+	Large-scale production apps that scale
+	to million of users
+
+	Circuit breaking
+*/
+
+func main() {
+	// Initialize the logger middleware
+	logger := middleware.Chain(
+		middleware.Recovery(),
+		middleware.Trace(),
+		middleware.Log(log.DefaultLogger),
+	)
+
+	// Create a service with the logger middleware
+	app := kratos.New(
+		kratos.Name("my-service"),
+		kratos.Server(
+			http.NewServer(":8000", logger),
+		),
+	)
+	app.Run()
+}
